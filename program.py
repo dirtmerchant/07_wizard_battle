2c03687f7bb7303a7700486c886e22d2b13c12be
Bert Bullough <bert@bertbullough.com>
2018-06-20 21:17:59 -0600

    added behaviors

--- program.py
+++ program.py
@@ -1,3 +1,7 @@
+import random
+
+import time
+
 from actors import Wizard, Creatures
 
 
@@ -26,16 +30,31 @@ def game_loop():
 
     while True:
 
+        active_creature = random.choice(creatures)
+        print('A {} of level {} has appeared from a dark and foggy forest... '
+              .format(active_creature.name, active_creature.level))
+        print()
+
         cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around?')
         if cmd == 'a':
-            print('attack')
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print("The wizard runs and hides taking time to recover...")
+                time.sleep(5)
+                print("The wizard returns revitalized!")
         elif cmd == 'r':
-            print('runaway')
+            print('The wizard has become unsure of his power and flees.')
         elif cmd == 'l':
-            print('look around')
+            print("The wizard {} takes in the surroundings and sees:"
+                  .format(hero.name))
+            for c in creatures:
+                print(' * A {} of level {}'.format(c.name, c.level))
         else:
             print('Ok Exiting game. Bye.')
             break
+
+        print()
 
 
 if __name__ == '__main__':
